56948f54865acdaecdf6c2193ef32e8df71a67e8
Alen Dvorak <alen.dvorak@gmail.com>
2018-07-13 23:27:04 +0200

    Prepare directory data structure

--- src/main/java/dvoraka/architecturebuilder/DirType.java
+++ src/main/java/dvoraka/architecturebuilder/DirType.java
@@ -10,28 +10,34 @@ public enum DirType {
     SRC_ROOT,
     SRC_TEST_ROOT,
 
-    SRC_BASE,
-    SRC_TEST_BASE,
+    SRC_BASE(false, true),
+    SRC_TEST_BASE(false, true),
 
     SRC_PROPERTIES,
     SRC_TEST_PROPERTIES,
 
-    SERVICE_ABSTRACT(true),
+    SERVICE_ABSTRACT(true, false),
     SERVICE,
     SERVICE_IMPL;
 
     private final boolean abstractType;
+    private final boolean baseType;
 
 
     DirType() {
-        this(false);
+        this(false, false);
     }
 
-    DirType(boolean abstractType) {
+    DirType(boolean abstractType, boolean baseType) {
         this.abstractType = abstractType;
+        this.baseType = baseType;
     }
 
     public boolean isAbstractType() {
         return abstractType;
+    }
+
+    public boolean isBaseType() {
+        return baseType;
     }
 }

f7816c7b9ea2c1c1fb6eb08380cadb9141254f50
dvoraka <alen.dvorak@gmail.com>
2018-07-21 15:06:55 +0200

    #2 Prepare generator

--- src/main/java/dvoraka/architecturebuilder/JavaGenerator.java
+++ src/main/java/dvoraka/architecturebuilder/JavaGenerator.java
@@ -1,7 +1,12 @@
 package dvoraka.architecturebuilder;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.EnumMap;
+import java.util.function.Consumer;
 
 import static java.util.Objects.requireNonNull;
 
@@ -10,10 +15,26 @@ public class JavaGenerator implements Generator {
 
     private final DirService dirService;
 
+    private final Logger log = LoggerFactory.getLogger(JavaGenerator.class);
+
+    private final EnumMap<DirType, Consumer<Directory>> conf;
+
 
     @Autowired
     public JavaGenerator(DirService dirService) {
         this.dirService = requireNonNull(dirService);
+
+        conf = new EnumMap<>(DirType.class);
+
+        checkImplementation();
+    }
+
+    private void checkImplementation() {
+        for (DirType type : DirType.values()) {
+            if (!conf.containsKey(type)) {
+                log.debug("Type not implemented: {}", type.toString());
+            }
+        }
     }
 
     @Override

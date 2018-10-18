696ea96277a5d25641857d1d48b468cb53d8286c
dvoraka <alen.dvorak@gmail.com>
2018-10-19 00:38:15 +0200

    #6 Prepare integration tests

---
+++ src/integration-test/groovy/dvoraka/archbuilder/generate/MainGeneratorISpec.groovy
@@ -0,0 +1,11 @@
+package dvoraka.archbuilder.generate
+
+import spock.lang.Specification
+
+class MainGeneratorISpec extends Specification {
+
+    def "test"() {
+        expect:
+            true
+    }
+}

2b17f0d90f00e5b3fdac718344892085cc3f1402
Alen Dvorak <alen.dvorak@gmail.com>
2018-10-20 11:41:27 +0200

    #6 Improve testing

--- src/integration-test/groovy/dvoraka/archbuilder/generate/MainGeneratorISpec.groovy
+++ src/integration-test/groovy/dvoraka/archbuilder/generate/MainGeneratorISpec.groovy
@@ -4,7 +4,12 @@ import dvoraka.archbuilder.DirType
 import dvoraka.archbuilder.Directory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import spock.lang.Shared
 import spock.lang.Specification
+
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.Paths
 
 @SpringBootTest
 class MainGeneratorISpec extends Specification {
@@ -12,12 +17,15 @@ class MainGeneratorISpec extends Specification {
     @Autowired
     Generator mainGenerator
 
+    @Shared
+    String rootDirName = "testRootDir"
+
     Directory root
     Directory srcRoot
 
 
     def setup() {
-        root = new Directory.DirectoryBuilder("rootDir")
+        root = new Directory.DirectoryBuilder(rootDirName)
                 .type(DirType.ROOT)
                 .parent(null)
                 .build()
@@ -26,6 +34,19 @@ class MainGeneratorISpec extends Specification {
                 .type(DirType.SRC_ROOT)
                 .parent(root)
                 .build()
+    }
+
+    def cleanup() {
+        Path path = Paths.get(rootDirName)
+        if (Files.notExists(path)) {
+            return
+        }
+
+        Files.walk(path)
+                .sorted(Comparator.reverseOrder())
+                .map(Path.&toFile)
+                .forEach(System.out.&print)
+//                .forEach(File.&delete)
     }
 
     def "test"() {

7d4aa803769e74bfdfa6a944af520a7d6c2793a7
dvoraka <alen.dvorak@gmail.com>
2018-12-02 18:56:23 +0100

    #12 Prepare implementation module

---
+++ src/integration-test/groovy/dvoraka/archbuilder/generate/ImplementationISpec.groovy
@@ -0,0 +1,65 @@
+package dvoraka.archbuilder.generate
+
+import dvoraka.archbuilder.DirType
+import dvoraka.archbuilder.Directory
+import org.springframework.beans.factory.annotation.Autowired
+
+class ImplementationISpec extends BaseISpec {
+
+    @Autowired
+    Generator mainGenerator
+
+
+    def "test"() {
+        expect:
+            true
+    }
+
+    def "List implementation"() {
+        given:
+            Directory abstractList = new Directory.DirectoryBuilder("component")
+                    .type(DirType.ABSTRACT)
+                    .parent(srcBase)
+                    .typeName("java.util.List")
+                    .build()
+            Directory listImpl = new Directory.DirectoryBuilder("component")
+                    .type(DirType.IMPL)
+                    .parent(srcBase)
+                    .superType(abstractList)
+                    .filename("CoolList")
+                    .parameterType("java.lang.Integer")
+                    .build()
+        when:
+            mainGenerator.generate(root)
+            Class<?> clazz = loadClass(getClassName(listImpl))
+        then:
+            notThrown(Exception)
+            isPublicNotAbstract(clazz)
+            hasNoTypeParameters(clazz)
+            hasDeclaredMethods(clazz)
+    }
+
+    def "Object abstract implementation"() {
+        given:
+            Directory abstractObject = new Directory.DirectoryBuilder("component")
+                    .type(DirType.ABSTRACT)
+                    .parent(srcBase)
+                    .typeName("java.lang.Object")
+                    .build()
+            Directory absObjectImpl = new Directory.DirectoryBuilder("component")
+                    .type(DirType.IMPL)
+                    .parent(srcBase)
+                    .superType(abstractObject)
+                    .abstractType()
+                    .filename("AbstractCoolObject")
+                    .build()
+        when:
+            mainGenerator.generate(root)
+            Class<?> clazz = loadClass(getClassName(absObjectImpl))
+        then:
+            notThrown(Exception)
+            isPublicAbstract(clazz)
+            hasNoTypeParameters(clazz)
+            hasNoDeclaredMethods(clazz)
+    }
+}
--- src/integration-test/groovy/dvoraka/archbuilder/generate/MainGeneratorISpec.groovy
+++ src/integration-test/groovy/dvoraka/archbuilder/generate/MainGeneratorISpec.groovy
@@ -119,52 +119,6 @@ class MainGeneratorISpec extends BaseISpec {
             notThrown(Exception)
     }
 
-    def "list implementation"() {
-        given:
-            Directory abstractList = new Directory.DirectoryBuilder("component")
-                    .type(DirType.ABSTRACT)
-                    .parent(srcBase)
-                    .typeName("java.util.List")
-                    .build()
-            Directory listImpl = new Directory.DirectoryBuilder("component")
-                    .type(DirType.IMPL)
-                    .parent(srcBase)
-                    .superType(abstractList)
-                    .filename("CoolList")
-                    .parameterType("java.lang.Integer")
-                    .build()
-        when:
-            mainGenerator.generate(root)
-            Class<?> clazz = loadClass(getClassName(listImpl))
-        then:
-            notThrown(Exception)
-            isPublicNotAbstract(clazz)
-            hasNoTypeParameters(clazz)
-    }
-
-    def "abstract object implementation"() {
-        given:
-            Directory abstractObject = new Directory.DirectoryBuilder("component")
-                    .type(DirType.ABSTRACT)
-                    .parent(srcBase)
-                    .typeName("java.lang.Object")
-                    .build()
-            Directory absObjectImpl = new Directory.DirectoryBuilder("component")
-                    .type(DirType.IMPL)
-                    .parent(srcBase)
-                    .superType(abstractObject)
-                    .abstractType()
-                    .filename("AbstractCoolObject")
-                    .build()
-        when:
-            mainGenerator.generate(root)
-            Class<?> clazz = loadClass(getClassName(absObjectImpl))
-        then:
-            notThrown(Exception)
-            isPublicAbstract(clazz)
-            hasNoTypeParameters(clazz)
-    }
-
     def "object implementation"() {
         given:
             Directory abstractObject = new Directory.DirectoryBuilder("component")

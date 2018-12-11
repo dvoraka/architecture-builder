cc788c29fa3d780dc75783953d24184b7caab6c3
dvoraka <alen.dvorak@gmail.com>
2018-12-11 22:06:41 +0100

    #12 Add new testing data

---
+++ src/main/java/dvoraka/archbuilder/test/Interface1p1m.java
@@ -0,0 +1,6 @@
+package dvoraka.archbuilder.test;
+
+public interface Interface1p1m<C> {
+
+    C get();
+}
---
+++ src/main/java/dvoraka/archbuilder/test/ServiceInterface2p.java
@@ -0,0 +1,6 @@
+package dvoraka.archbuilder.test;
+
+public interface ServiceInterface2p<A, B> extends Interface1p1m<B> {
+
+    void process(A data);
+}

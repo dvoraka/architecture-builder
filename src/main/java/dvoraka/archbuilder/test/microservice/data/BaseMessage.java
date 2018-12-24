200102f91a46da01fa2f019bf5c084f85f02e53d
dvoraka <alen.dvorak@gmail.com>
2018-12-24 14:35:13 +0100

    #14 Improve test data

---
+++ src/main/java/dvoraka/archbuilder/test/microservice/data/BaseMessage.java
@@ -0,0 +1,26 @@
+package dvoraka.archbuilder.test.microservice.data;
+
+import java.time.Instant;
+import java.util.UUID;
+
+public abstract class BaseMessage implements Message {
+
+    protected String id;
+    protected Instant timestamp;
+
+
+    protected BaseMessage() {
+        this.id = UUID.randomUUID().toString();
+        this.timestamp = Instant.now();
+    }
+
+    @Override
+    public String getId() {
+        return id;
+    }
+
+    @Override
+    public Instant getTimestamp() {
+        return timestamp;
+    }
+}
--- src/main/java/dvoraka/archbuilder/test/microservice/data/Message.java
+++ src/main/java/dvoraka/archbuilder/test/microservice/data/Message.java
@@ -1,4 +1,10 @@
 package dvoraka.archbuilder.test.microservice.data;
 
+import java.time.Instant;
+
 public interface Message {
+
+    String getId();
+
+    Instant getTimestamp();
 }

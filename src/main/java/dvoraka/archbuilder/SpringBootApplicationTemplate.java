eef594989b5794123acb19188ac868c064025a01
dvoraka <alen.dvorak@gmail.com>
2019-01-03 02:07:44 +0100

    #14 Source code template tuning

--- src/main/java/dvoraka/archbuilder/SpringBootApplicationTemplate.java
+++ src/main/java/dvoraka/archbuilder/SpringBootApplicationTemplate.java
@@ -9,32 +9,45 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import javax.lang.model.element.Modifier;
 
+import static java.util.Objects.requireNonNull;
+
 public class SpringBootApplicationTemplate implements SourceTemplate {
 
-    //TODO
+    private final String className;
+    private final String packageName;
+
+
+    public SpringBootApplicationTemplate(String className, String packageName) {
+        this.className = requireNonNull(className);
+        this.packageName = requireNonNull(packageName);
+    }
 
     @Override
     public String getSource() {
-
-        String className = "CustomType";
-        String packageName = "";
         String argsName = "args";
-
         // public static void main
         MethodSpec methodSpec = MethodSpec.methodBuilder("main")
                 .addModifiers(Modifier.PUBLIC, Modifier.STATIC)
                 .returns(Void.TYPE)
                 .addParameter(ArrayTypeName.of(String.class), argsName)
-                .addStatement("$T.run($L.class, $L)", SpringApplication.class, className, argsName)
+                .addStatement("$T.run($L.class, $L)", SpringApplication.class, getClassName(), argsName)
                 .build();
-        TypeSpec typeSpec = TypeSpec.classBuilder(className)
+        TypeSpec typeSpec = TypeSpec.classBuilder(getClassName())
                 .addModifiers(Modifier.PUBLIC)
                 .addAnnotation(SpringBootApplication.class)
                 .addMethod(methodSpec)
                 .build();
-        JavaFile javaFile = JavaFile.builder(packageName, typeSpec)
+        JavaFile javaFile = JavaFile.builder(getPackageName(), typeSpec)
                 .build();
 
         return javaFile.toString();
+    }
+
+    private String getClassName() {
+        return className;
+    }
+
+    private String getPackageName() {
+        return packageName;
     }
 }

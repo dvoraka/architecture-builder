3d7fca7ed9e29936b8722eb3784ae68415fcd157
dvoraka <alen.dvorak@gmail.com>
2019-01-09 02:12:36 +0100

    #14 Prepare network component abstraction

--- src/main/java/dvoraka/archbuilder/test/microservice/net/NetworkComponent.java
+++ src/main/java/dvoraka/archbuilder/test/microservice/net/NetworkComponent.java
@@ -1,4 +1,9 @@
 package dvoraka.archbuilder.test.microservice.net;
 
-public interface NetworkComponent {
+import dvoraka.archbuilder.test.microservice.data.ResultData;
+import dvoraka.archbuilder.test.microservice.data.message.ResultMessage;
+
+public interface NetworkComponent<R extends ResultMessage<D>, D extends ResultData> {
+
+    void send(R resultMessage);
 }

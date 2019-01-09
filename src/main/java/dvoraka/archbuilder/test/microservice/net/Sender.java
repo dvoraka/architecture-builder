9faf394d00b7bc56498862824a0ceb62ea08777e
Alen Dvorak <alen.dvorak@gmail.com>
2019-01-10 00:27:00 +0100

    #20 Improve network abstraction

--- src/main/java/dvoraka/archbuilder/test/microservice/net/Sender.java
+++ src/main/java/dvoraka/archbuilder/test/microservice/net/Sender.java
@@ -2,6 +2,7 @@ package dvoraka.archbuilder.test.microservice.net;
 
 import dvoraka.archbuilder.test.microservice.data.message.Message;
 
-public interface NetworkComponent<M extends Message, R extends Message>
-        extends Sender<M>, Receiver<R> {
+public interface Sender<M extends Message> {
+
+    void send(M message) throws Exception;
 }

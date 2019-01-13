b1ee9c81d5dd7b9e811e46f953d3255942bd0908
dvoraka <alen.dvorak@gmail.com>
2019-01-13 13:25:53 +0100

    #23 Prepare build tool

--- src/main/java/dvoraka/archbuilder/GradleBuildTool.java
+++ src/main/java/dvoraka/archbuilder/GradleBuildTool.java
@@ -1,10 +1,16 @@
 package dvoraka.archbuilder;
 
+import org.gradle.tooling.BuildLauncher;
+import org.gradle.tooling.GradleConnector;
+import org.gradle.tooling.ProjectConnection;
+
 import java.io.File;
 
 import static java.util.Objects.requireNonNull;
 
 public class GradleBuildTool implements BuildTool {
+
+    public static final String PREPARE_ENV_CMD = "wrapper";
 
     private final File path;
 
@@ -15,7 +21,14 @@ public class GradleBuildTool implements BuildTool {
 
     @Override
     public void prepareEnv() {
+        ProjectConnection connection = createProjectConnection();
 
+        BuildLauncher buildLauncher = connection.newBuild();
+        buildLauncher.setStandardOutput(System.out);
+        buildLauncher.forTasks(PREPARE_ENV_CMD);
+        buildLauncher.run();
+
+        connection.close();
     }
 
     @Override
@@ -30,5 +43,11 @@ public class GradleBuildTool implements BuildTool {
 
     public File getPath() {
         return path;
+    }
+
+    private ProjectConnection createProjectConnection() {
+        return GradleConnector.newConnector()
+                .forProjectDirectory(getPath())
+                .connect();
     }
 }
--- src/main/java/dvoraka/archbuilder/GradleToolingApiApp.java
+++ src/main/java/dvoraka/archbuilder/GradleToolingApiApp.java
@@ -13,24 +13,26 @@ public class GradleToolingApiApp {
 
     public static void main(String[] args) {
 
-        try (ProjectConnection connection = GradleConnector.newConnector()
-                // gradle root of the new project (with settings.gradle file)
-                .forProjectDirectory(new File("testRoot"))
-                // you can set Gradle version, default is current project's version
-//                .useDistribution(URI.create("https://services.gradle.org/distributions/gradle-5.0-all.zip"))
-                .connect()) {
+//        try (ProjectConnection connection = GradleConnector.newConnector()
+//                // gradle root of the new project (with settings.gradle file)
+//                .forProjectDirectory(new File("testRoot"))
+//                // you can set Gradle version, default is current project's version
+////                .useDistribution(URI.create("https://services.gradle.org/distributions/gradle-5.0-all.zip"))
+//                .connect()) {
+//
+//            GradleProject project = connection.getModel(GradleProject.class);
+//
+//            DomainObjectSet<? extends GradleTask> tasks = project.getTasks();
+//            tasks.forEach(System.out::println);
+//
+//            BuildLauncher buildLauncher = connection.newBuild();
+////                buildLauncher.addProgressListener((ProgressListener) System.out::println);
+//            buildLauncher.setStandardOutput(System.out);
+//            buildLauncher.forTasks("wrapper");
+//
+//            buildLauncher.run();
 
-            GradleProject project = connection.getModel(GradleProject.class);
-
-            DomainObjectSet<? extends GradleTask> tasks = project.getTasks();
-            tasks.forEach(System.out::println);
-
-            BuildLauncher buildLauncher = connection.newBuild();
-//                buildLauncher.addProgressListener((ProgressListener) System.out::println);
-            buildLauncher.setStandardOutput(System.out);
-            buildLauncher.forTasks("wrapper");
-
-            buildLauncher.run();
-        }
+        BuildTool buildTool = new GradleBuildTool(new File("."));
+        buildTool.prepareEnv();
     }
 }

f58ceca5491e63758feb690980e6dd4e57cee9c5
Alen Dvorak <alen.dvorak@gmail.com>
2019-01-20 00:02:47 +0100

    #12 Add new tests

--- src/integration-test/groovy/dvoraka/archbuilder/generate/GenericsISpec.groovy
+++ src/integration-test/groovy/dvoraka/archbuilder/generate/GenericsISpec.groovy
@@ -4,6 +4,7 @@ import dvoraka.archbuilder.DirType
 import dvoraka.archbuilder.Directory
 import dvoraka.archbuilder.sample.generic.Interface2p2am
 import dvoraka.archbuilder.sample.generic.Interface2pb2am
+import dvoraka.archbuilder.sample.generic.InterfaceE2p2am
 import org.springframework.beans.factory.annotation.Autowired
 
 class GenericsISpec extends BaseISpec {
@@ -41,6 +42,56 @@ class GenericsISpec extends BaseISpec {
     def "interface 2p2am implementation NP"() {
         given:
             Class<?> cls = Interface2p2am
+            Directory abs = new Directory.DirectoryBuilder('test')
+                    .type(DirType.ABSTRACT)
+                    .parent(srcBase)
+                    .typeClass(cls)
+                    .build()
+            Directory impl = new Directory.DirectoryBuilder('generics')
+                    .type(DirType.IMPL)
+                    .parent(srcBase)
+                    .superType(abs)
+                    .filename('TestNP' + cls.getSimpleName())
+                    .build()
+        when:
+            mainGenerator.generate(root)
+            Class<?> clazz = loadClass(getClassName(impl))
+        then:
+            notThrown(Exception)
+            isPublicNotAbstract(clazz)
+            hasTypeParameters(clazz)
+            declaredMethodCount(clazz) == 2
+    }
+
+    def "interface E2p2am implementation"() {
+        given:
+            Class<?> cls = InterfaceE2p2am
+            Directory abs = new Directory.DirectoryBuilder('test')
+                    .type(DirType.ABSTRACT)
+                    .parent(srcBase)
+                    .typeClass(cls)
+                    .build()
+            Directory impl = new Directory.DirectoryBuilder('generics')
+                    .type(DirType.IMPL)
+                    .parent(srcBase)
+                    .superType(abs)
+                    .filename('Test' + cls.getSimpleName())
+                    .parameterTypeClass(Long.class)
+                    .parameterTypeClass(String.class)
+                    .build()
+        when:
+            mainGenerator.generate(root)
+            Class<?> clazz = loadClass(getClassName(impl))
+        then:
+            notThrown(Exception)
+            isPublicNotAbstract(clazz)
+            hasNoTypeParameters(clazz)
+            declaredMethodCount(clazz) == 2
+    }
+
+    def "interface E2p2am implementation NP"() {
+        given:
+            Class<?> cls = InterfaceE2p2am
             Directory abs = new Directory.DirectoryBuilder('test')
                     .type(DirType.ABSTRACT)
                     .parent(srcBase)
---
+++ src/main/java/dvoraka/archbuilder/sample/generic/InterfaceE2p2am.java
@@ -0,0 +1,8 @@
+package dvoraka.archbuilder.sample.generic;
+
+import java.util.Collection;
+
+public interface InterfaceE2p2am<A, B> extends Interface1p1am<A> {
+
+    Collection<B> second(B data);
+}

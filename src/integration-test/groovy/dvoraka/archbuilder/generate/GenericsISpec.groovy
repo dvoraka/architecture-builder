3538c13430fe619973c6c9befe264910ce5ad135
dvoraka <alen.dvorak@gmail.com>
2019-01-19 22:32:21 +0100

    #12 Prepare new generics testing

---
+++ src/integration-test/groovy/dvoraka/archbuilder/generate/GenericsISpec.groovy
@@ -0,0 +1,39 @@
+package dvoraka.archbuilder.generate
+
+import dvoraka.archbuilder.DirType
+import dvoraka.archbuilder.Directory
+import dvoraka.archbuilder.sample.generic.Interface2p2am
+import org.springframework.beans.factory.annotation.Autowired
+
+class GenericsISpec extends BaseISpec {
+
+    @Autowired
+    Generator mainGenerator
+
+
+    def "interface 2p2am implementation"() {
+        given:
+            Class<?> cls = Interface2p2am
+            Directory abs = new Directory.DirectoryBuilder('test')
+                    .type(DirType.ABSTRACT)
+                    .parent(srcBase)
+                    .typeClass(cls)
+                    .build()
+            Directory impl = new Directory.DirectoryBuilder('generics')
+                    .type(DirType.IMPL)
+                    .parent(srcBase)
+                    .superType(abs)
+                    .filename('Test' + cls.getSimpleName())
+                    .parameterTypeClass(Long.class)
+                    .parameterTypeClass(String.class)
+                    .build()
+        when:
+            mainGenerator.generate(root)
+            Class<?> clazz = loadClass(getClassName(impl))
+        then:
+            notThrown(Exception)
+            isPublicNotAbstract(clazz)
+            hasNoTypeParameters(clazz)
+            declaredMethodCount(clazz) == 2
+    }
+}

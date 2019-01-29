f1926d9c6a1e4f1e786ee2c6c4d0f7a571ba4182
dvoraka <alen.dvorak@gmail.com>
2019-01-29 22:37:11 +0100

    #14 Improve server abstraction

--- data/src/main/java/dvoraka.archbuilder/sample/microservice/server/AbstractServer.java
+++ data/src/main/java/dvoraka.archbuilder/sample/microservice/server/AbstractServer.java
@@ -1,11 +1,16 @@
 package dvoraka.archbuilder.sample.microservice.server;
 
 import dvoraka.archbuilder.sample.microservice.Management;
+import dvoraka.archbuilder.sample.microservice.net.Acknowledgment;
 
 public abstract class AbstractServer implements Management {
 
     private volatile boolean running;
 
+
+    public abstract void start();
+
+    public abstract void stop();
 
     @Override
     public boolean isRunning() {
@@ -14,5 +19,9 @@ public abstract class AbstractServer implements Management {
 
     protected void setRunning(boolean running) {
         this.running = running;
+    }
+
+    protected void ackMessage(Acknowledgment acknowledgment) {
+        acknowledgment.ack();
     }
 }

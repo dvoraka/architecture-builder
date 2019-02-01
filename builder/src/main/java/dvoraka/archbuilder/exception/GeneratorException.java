9392c169c49173bc5d20993c25a088c63ded3613
dvoraka <alen.dvorak@gmail.com>
2019-02-01 14:27:25 +0100

    Add base exception

---
+++ builder/src/main/java/dvoraka/archbuilder/exception/GeneratorException.java
@@ -0,0 +1,11 @@
+package dvoraka.archbuilder.exception;
+
+public class GeneratorException extends RuntimeException {
+
+    private static final long serialVersionUID = -7453870399398662172L;
+
+
+    public GeneratorException(String message) {
+        super(message);
+    }
+}

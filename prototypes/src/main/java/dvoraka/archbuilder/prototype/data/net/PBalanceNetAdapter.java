d06473b0ae5f8a640a57b3b89a54437e3210aff5
dvoraka <alen.dvorak@gmail.com>
2019-02-09 12:52:15 +0100

    #36 Prepare RabbitMQ adapter prototype

--- prototypes/src/main/java/dvoraka/archbuilder/prototype/data/net/PBalanceNetAdapter.java
+++ prototypes/src/main/java/dvoraka/archbuilder/prototype/data/net/PBalanceNetAdapter.java
@@ -4,12 +4,30 @@ import dvoraka.archbuilder.prototype.data.PBalanceData;
 import dvoraka.archbuilder.prototype.data.PBalanceException;
 import dvoraka.archbuilder.prototype.data.message.PBalanceMessage;
 import dvoraka.archbuilder.prototype.data.message.PBalanceResponseMessage;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessageListener;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 
 public class PBalanceNetAdapter
         extends PBaseNetComponent<PBalanceMessage, PBalanceResponseMessage, PBalanceData, PBalanceException>
-        implements PBalanceNetComponent {
+        implements PBalanceNetComponent, MessageListener {
+
+    private RabbitTemplate rabbitTemplate;
+
 
     @Override
     public void send(PBalanceResponseMessage message) throws Exception {
+        rabbitTemplate.send(null);
+    }
+
+    @Override
+    public void onMessage(Message message) {
+        onMessage(null, null);
+    }
+
+    @RabbitListener
+    public void receive() {
+
     }
 }

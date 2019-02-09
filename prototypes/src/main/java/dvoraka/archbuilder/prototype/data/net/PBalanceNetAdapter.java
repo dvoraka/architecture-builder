5b537a222b8fc92ff1394930acc2cc7265071774
dvoraka <alen.dvorak@gmail.com>
2019-02-09 02:36:38 +0100

    #36 Prepare network abstraction

--- prototypes/src/main/java/dvoraka/archbuilder/prototype/data/net/PBalanceNetAdapter.java
+++ prototypes/src/main/java/dvoraka/archbuilder/prototype/data/net/PBalanceNetAdapter.java
@@ -1,29 +1,15 @@
 package dvoraka.archbuilder.prototype.data.net;
 
+import dvoraka.archbuilder.prototype.data.PBalanceData;
+import dvoraka.archbuilder.prototype.data.PBalanceException;
 import dvoraka.archbuilder.prototype.data.message.PBalanceMessage;
 import dvoraka.archbuilder.prototype.data.message.PBalanceResponseMessage;
-import dvoraka.archbuilder.sample.microservice.net.Acknowledgment;
-import dvoraka.archbuilder.sample.microservice.net.NetMessageListener;
 
-public class PBalanceNetAdapter implements PBalanceNetComponent {
-
-    @Override
-    public void addMessageListener(NetMessageListener<PBalanceMessage> listener) {
-
-    }
-
-    @Override
-    public void removeMessageListener(NetMessageListener<PBalanceMessage> listener) {
-
-    }
-
-    @Override
-    public void onMessage(PBalanceMessage message, Acknowledgment acknowledgment) {
-
-    }
+public class PBalanceNetAdapter
+        extends PBaseNetComponent<PBalanceMessage, PBalanceResponseMessage, PBalanceData, PBalanceException>
+        implements PBalanceNetComponent {
 
     @Override
     public void send(PBalanceResponseMessage message) throws Exception {
-
     }
 }
--- prototypes/src/main/java/dvoraka/archbuilder/prototype/data/net/PBaseNetComponent.java
+++ prototypes/src/main/java/dvoraka/archbuilder/prototype/data/net/PBaseNetComponent.java
@@ -1,10 +1,16 @@
 package dvoraka.archbuilder.prototype.data.net;
 
+import dvoraka.archbuilder.sample.microservice.data.BaseException;
+import dvoraka.archbuilder.sample.microservice.data.ResultData;
 import dvoraka.archbuilder.sample.microservice.data.message.Message;
 import dvoraka.archbuilder.sample.microservice.data.message.ResponseMessage;
-import dvoraka.archbuilder.sample.microservice.net.GenericNetComponent;
+import dvoraka.archbuilder.sample.microservice.net.ServiceNetComponent;
 
-public abstract class PBaseNetComponent<M extends Message, R extends ResponseMessage>
+public abstract class PBaseNetComponent<
+        M extends Message,
+        R extends ResponseMessage<D, E>,
+        D extends ResultData<E>,
+        E extends BaseException>
         extends PBaseNetReceiver<M>
-        implements GenericNetComponent<M, R> {
+        implements ServiceNetComponent<M, R, D, E> {
 }

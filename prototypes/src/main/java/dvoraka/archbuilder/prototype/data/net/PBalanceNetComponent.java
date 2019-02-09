4d200257f35399c77875d050ae07ab670581484e
dvoraka <alen.dvorak@gmail.com>
2019-02-09 01:32:03 +0100

    #36 Prepare network abstraction

--- prototypes/src/main/java/dvoraka/archbuilder/prototype/data/net/PBalanceNetComponent.java
+++ prototypes/src/main/java/dvoraka/archbuilder/prototype/data/net/PBalanceNetComponent.java
@@ -4,33 +4,14 @@ import dvoraka.archbuilder.prototype.data.PBalanceData;
 import dvoraka.archbuilder.prototype.data.PBalanceException;
 import dvoraka.archbuilder.prototype.data.message.PBalanceMessage;
 import dvoraka.archbuilder.prototype.data.message.PBalanceResponseMessage;
-import dvoraka.archbuilder.sample.microservice.net.Acknowledgment;
-import dvoraka.archbuilder.sample.microservice.net.NetMessageListener;
 import dvoraka.archbuilder.sample.microservice.net.ServiceNetComponent;
 
-public class PBalanceNetComponent implements ServiceNetComponent<
+/**
+ * Balance network component interface.
+ */
+public interface PBalanceNetComponent extends ServiceNetComponent<
         PBalanceMessage,
         PBalanceResponseMessage,
         PBalanceData,
         PBalanceException> {
-
-    @Override
-    public void addMessageListener(NetMessageListener<PBalanceMessage> listener) {
-
-    }
-
-    @Override
-    public void removeMessageListener(NetMessageListener<PBalanceMessage> listener) {
-
-    }
-
-    @Override
-    public void onMessage(PBalanceMessage message, Acknowledgment acknowledgment) {
-
-    }
-
-    @Override
-    public void send(PBalanceResponseMessage message) throws Exception {
-
-    }
 }
--- prototypes/src/main/java/dvoraka/archbuilder/prototype/net/NetComponentApp.java
+++ prototypes/src/main/java/dvoraka/archbuilder/prototype/net/NetComponentApp.java
@@ -1,8 +1,13 @@
 package dvoraka.archbuilder.prototype.net;
 
+import dvoraka.archbuilder.prototype.data.net.PBalanceNetAdapter;
+import dvoraka.archbuilder.prototype.data.net.PBalanceNetComponent;
+
 public class NetComponentApp {
 
     public static void main(String[] args) {
         System.out.println("Net component app");
+
+        PBalanceNetComponent netComponent = new PBalanceNetAdapter();
     }
 }

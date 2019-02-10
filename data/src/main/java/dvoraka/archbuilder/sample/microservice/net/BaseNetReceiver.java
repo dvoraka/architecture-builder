923e5a06a9e5520dfd993f728cb11f30f71e93e3
dvoraka <alen.dvorak@gmail.com>
2019-02-10 21:12:56 +0100

    #37 Merge net receiver data structures

--- data/src/main/java/dvoraka/archbuilder/sample/microservice/net/BaseNetReceiver.java
+++ data/src/main/java/dvoraka/archbuilder/sample/microservice/net/BaseNetReceiver.java
@@ -1,24 +1,40 @@
 package dvoraka.archbuilder.sample.microservice.net;
 
 import dvoraka.archbuilder.sample.microservice.data.message.Message;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Set;
 import java.util.concurrent.CopyOnWriteArraySet;
-
-//TODO: merge with abstract net receiver
 
 /**
  * Base class for network receiver.
  *
  * @param <M> the receiving message type
  */
-public class BaseNetReceiver<M extends Message> implements NetReceiver<M> {
+public abstract class BaseNetReceiver<M extends Message> implements NetReceiver<M> {
+
+    protected final Logger log = LoggerFactory.getLogger(this.getClass());
 
     private final Set<NetMessageListener<M>> listeners;
 
 
     public BaseNetReceiver() {
         listeners = new CopyOnWriteArraySet<>();
+    }
+
+    @Override
+    public void onMessage(M message, Acknowledgment acknowledgment) {
+        try {
+            handleOnMessage(message, acknowledgment);
+        } catch (Exception e) {
+            log.error("On message failed!", e);
+        }
+    }
+
+    private void handleOnMessage(M message, Acknowledgment acknowledgment) {
+        log.debug("On message: {}", message);
+        listeners.forEach(listener -> listener.onMessage(message, acknowledgment));
     }
 
     @Override
@@ -29,10 +45,5 @@ public class BaseNetReceiver<M extends Message> implements NetReceiver<M> {
     @Override
     public void removeMessageListener(NetMessageListener<M> listener) {
         listeners.remove(listener);
-    }
-
-    @Override
-    public void onMessage(M message, Acknowledgment acknowledgment) {
-        listeners.forEach(listener -> listener.onMessage(message, acknowledgment));
     }
 }

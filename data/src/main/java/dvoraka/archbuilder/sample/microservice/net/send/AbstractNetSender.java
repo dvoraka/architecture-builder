abd740c9c50ef86f4aa565dbf053bc319df0eec1
dvoraka <alen.dvorak@gmail.com>
2019-02-23 11:52:44 +0100

    #37 Remove redundant abstraction for sender

--- data/src/main/java/dvoraka/archbuilder/sample/microservice/net/send/AbstractNetSender.java
+++
@@ -1,6 +0,0 @@
-package dvoraka.archbuilder.sample.microservice.net.send;
-
-import dvoraka.archbuilder.sample.microservice.data.message.Message;
-
-public abstract class AbstractNetSender<M extends Message> implements NetSender<M> {
-}

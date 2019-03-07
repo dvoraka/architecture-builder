5b018fd55057b67af10a3eb22a255386a421926a
dvoraka <alen.dvorak@gmail.com>
2019-03-07 01:30:40 +0100

    #50 Improve substitution prototype

--- prototypes/src/main/java/dvoraka/archbuilder/prototype/TextApp.java
+++ prototypes/src/main/java/dvoraka/archbuilder/prototype/TextApp.java
@@ -8,26 +8,37 @@ public class TextApp {
     public static void main(String[] args) {
 
         String var1Name = "var1";
-//        String var2Name = "var2";
-        String var2Name = ".*var2"; // name must be checked for special characters
+        String var2Name = "var.*";
+        String var3Name = "var.+";
 
         String var1Value = "VALUE";
-        String var2Value = "ANOTHER VALUE";
+        String var2Value = "**..**";
+        String var3Value = "$$..$$";
+
+        System.out.println(Pattern.quote(var3Value));
+        System.out.println(Matcher.quoteReplacement(var3Value));
 
         String text = "Long text with vars and ${unknown vars}, var1: ${"
-                + var1Name + "}, var2: ${" + Matcher.quoteReplacement(var2Name) + "}, var1: " + "${" + var1Name + "}";
+                + var1Name
+                + "}, var2: ${"
+                + var2Name
+                + "}, var1: ${"
+                + var1Name
+                + "}, var3: ${"
+                + var3Name
+                + "}";
         System.out.println(text);
 
-        String update1 = text.replaceAll("\\$\\{" + var1Name + "}", var1Value);
+        String update1 = text.replaceAll("\\$\\{" + var1Name + "}",
+                Matcher.quoteReplacement(var1Value));
         System.out.println(update1);
 
-        String update2 = update1.replaceAll("\\$\\{" + Pattern.quote(var2Name) + "}", var2Value);
+        String update2 = update1.replaceAll("\\$\\{" + Pattern.quote(var2Name) + "}",
+                Matcher.quoteReplacement(var2Value));
         System.out.println(update2);
 
-        System.out.println();
-
-        String coolName = ".$.?.na&me";
-        System.out.println(Pattern.quote(coolName));
-        System.out.println(Matcher.quoteReplacement(coolName));
+        String update3 = update2.replaceAll("\\$\\{" + Pattern.quote(var3Name) + "}",
+                Matcher.quoteReplacement(var3Value));
+        System.out.println(update3);
     }
 }

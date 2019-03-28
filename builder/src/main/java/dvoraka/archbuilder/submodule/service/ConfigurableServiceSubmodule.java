b74c076272c1aa7a51dedbd13b2b418560d32848
dvoraka <alen.dvorak@gmail.com>
2019-03-28 14:42:39 +0100

    #53 Add simple data service generation

--- builder/src/main/java/dvoraka/archbuilder/submodule/service/ConfigurableServiceSubmodule.java
+++ builder/src/main/java/dvoraka/archbuilder/submodule/service/ConfigurableServiceSubmodule.java
@@ -7,11 +7,13 @@ import dvoraka.archbuilder.springconfig.BeanMapping;
 import dvoraka.archbuilder.springconfig.SpringConfigGenerator;
 import dvoraka.archbuilder.template.TemplateHelper;
 import dvoraka.archbuilder.util.JavaUtils;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
+import static dvoraka.archbuilder.util.JavaUtils.javaSuffix;
 import static dvoraka.archbuilder.util.Utils.noFilenameException;
 import static dvoraka.archbuilder.util.Utils.uncapitalize;
 
@@ -46,30 +48,58 @@ public class ConfigurableServiceSubmodule implements ServiceSubmodule, TemplateH
     public void addSubmoduleTo(Directory srcBase) {
 
         String serviceFullName = serviceName + "Service";
+        String serviceImplFullName = "Default" + serviceFullName;
+        String dataServiceFullName = serviceName + "DataService";
+        String dataServiceImplFullName = "Default" + serviceName + "DataService";
+
+        // service
         service = new Directory.Builder("service", DirType.SERVICE)
                 .parent(srcBase)
                 .superType(superService)
                 .filename(serviceFullName)
                 .parameterType(typeArguments)
                 .build();
-
-        String serviceImplFullName = "Default" + serviceFullName;
         Directory serviceImpl = new Directory.Builder("service", DirType.SERVICE_IMPL)
                 .parent(srcBase)
                 .superType(service)
                 .filename(serviceImplFullName)
                 .build();
 
+        // data service
+        Directory dataService = new Directory.Builder("service", DirType.NEW_TYPE)
+                .parent(srcBase)
+                .filename(javaSuffix(dataServiceFullName))
+                .interfaceType()
+                .build();
+        Directory dataServiceImpl = new Directory.Builder("service", DirType.IMPL)
+                .parent(srcBase)
+                .superType(dataService)
+                .filename(javaSuffix(dataServiceImplFullName))
+                .metadata(Service.class)
+                .build();
+
+        // Spring config
         String serviceFilename = service.getFilename()
                 .orElseThrow(() -> noFilenameException(service));
+        String dataServiceFilename = dataService.getFilename()
+                .orElseThrow(() -> noFilenameException(service));
+
         String serviceMappingName = uncapitalize(JavaUtils.removeJavaSuffix(serviceFilename));
+        String dataServiceMappingName = uncapitalize(JavaUtils.removeJavaSuffix(dataServiceFilename));
+
         BeanMapping serviceBeanMapping = new BeanMapping.Builder(serviceMappingName)
                 .typeDir(service)
                 .toTypeDir(serviceImpl)
                 .codeTemplate(configGenerator::simpleReturn)
                 .build();
+        BeanMapping dataServiceBeanMapping = new BeanMapping.Builder(dataServiceMappingName)
+                .typeDir(dataService)
+                .toTypeDir(dataServiceImpl)
+                .codeTemplate(configGenerator::simpleReturn)
+                .build();
 
         configuration.add(serviceBeanMapping);
+        configuration.add(dataServiceBeanMapping);
     }
 
     @Override

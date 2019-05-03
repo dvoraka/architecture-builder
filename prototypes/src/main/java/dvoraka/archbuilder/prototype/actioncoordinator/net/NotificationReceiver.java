f9d00372d76c043c0ccb6968f3cfb755f1b2e4e7
Alen Dvorak <alen.dvorak@gmail.com>
2019-05-03 20:56:26 +0200

    #65 Prepare notification components

---
+++ prototypes/src/main/java/dvoraka/archbuilder/prototype/actioncoordinator/net/NotificationReceiver.java
@@ -0,0 +1,7 @@
+package dvoraka.archbuilder.prototype.actioncoordinator.net;
+
+import dvoraka.archbuilder.sample.microservice.data.notification.Notification;
+import dvoraka.archbuilder.sample.microservice.net.receive.NetReceiver;
+
+public interface NotificationReceiver extends NetReceiver<Notification> {
+}
---
+++ prototypes/src/main/java/dvoraka/archbuilder/prototype/actioncoordinator/net/NotificationSender.java
@@ -0,0 +1,7 @@
+package dvoraka.archbuilder.prototype.actioncoordinator.net;
+
+import dvoraka.archbuilder.sample.microservice.data.notification.Notification;
+import dvoraka.archbuilder.sample.microservice.net.send.NetSender;
+
+public interface NotificationSender extends NetSender<Notification> {
+}

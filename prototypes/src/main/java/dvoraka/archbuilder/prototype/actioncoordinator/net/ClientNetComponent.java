57b955da612180d2e8d6316a80dfa07f3edaa1e3
dvoraka <alen.dvorak@gmail.com>
2019-10-27 19:47:34 +0100

    #65 Update client network component abstraction

--- prototypes/src/main/java/dvoraka/archbuilder/prototype/actioncoordinator/net/ClientNetComponent.java
+++ prototypes/src/main/java/dvoraka/archbuilder/prototype/actioncoordinator/net/ClientNetComponent.java
@@ -1,15 +1,20 @@
 package dvoraka.archbuilder.prototype.actioncoordinator.net;
 
 import dvoraka.archbuilder.sample.microservice.data.BaseException;
+import dvoraka.archbuilder.sample.microservice.data.ResultData;
 import dvoraka.archbuilder.sample.microservice.data.message.Message;
 import dvoraka.archbuilder.sample.microservice.data.message.ResultMessage;
 
 import java.util.function.Consumer;
 
-public interface ClientNetComponent<M extends Message, R extends ResultMessage, E, T, RD> {
+public interface ClientNetComponent<
+        M extends Message,
+        R extends ResultMessage<RD, E>,
+        E extends BaseException,
+        T,
+        RD extends ResultData<E>> {
 
-    //TODO: exception
-    void send(M message, Consumer<R> callback) throws BaseException;
+    void send(M message, Consumer<R> callback) throws E;
 
     void sendFast(M message, Consumer<R> callback);
 }

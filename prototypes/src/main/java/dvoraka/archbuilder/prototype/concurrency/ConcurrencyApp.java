8a449a598379d416b64f1f23d92ee864a8a3eb27
dvoraka <alen.dvorak@gmail.com>
2019-11-02 18:55:47 +0100

    #70 Prepare app

--- prototypes/src/main/java/dvoraka/archbuilder/prototype/concurrency/ConcurrencyApp.java
+++ prototypes/src/main/java/dvoraka/archbuilder/prototype/concurrency/ConcurrencyApp.java
@@ -16,23 +16,38 @@ public class ConcurrencyApp {
 
     /**
      * Scenario 1:
-     * <li>10 times task1 (unlimited thread count)</li>
+     * <li>6 times task1 (unlimited thread count)</li>
      */
     @Bean
     public CommandLineRunner runner() {
         return args -> {
             System.out.println("Concurrency app");
 
+            final int loops = 6;
+            final StopWatch stopWatch = new StopWatch("Concurrency app");
             Tasks tasks = new Tasks();
-            StopWatch stopWatch = new StopWatch("Concurrency app");
+
             warmup();
 
             // main thread
-            System.out.println("Main thread:");
+            System.out.println("main thread...");
             stopWatch.start("main thread");
-            for (int i = 0; i < 10; i++) {
+            for (int i = 0; i < loops; i++) {
                 tasks.task1();
             }
+            stopWatch.stop();
+
+            // new thread
+            System.out.println("new thread...");
+            stopWatch.start("new thread");
+            Runnable runnable = () -> {
+                for (int i = 0; i < loops; i++) {
+                    tasks.task1();
+                }
+            };
+            Thread thread1 = new Thread(runnable);
+            thread1.start();
+            thread1.join();
             stopWatch.stop();
 
             System.out.println(stopWatch.prettyPrint());

bdf7ea0f153192d3a980f9a03dd98ea218d9c882
dvoraka <alen.dvorak@gmail.com>
2019-11-02 19:06:27 +0100

    #70 Prepare app

--- prototypes/src/main/java/dvoraka/archbuilder/prototype/concurrency/ConcurrencyApp.java
+++ prototypes/src/main/java/dvoraka/archbuilder/prototype/concurrency/ConcurrencyApp.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.util.StopWatch;
 
+import java.util.concurrent.CompletableFuture;
+
 @SpringBootApplication
 public class ConcurrencyApp {
 
@@ -39,15 +41,25 @@ public class ConcurrencyApp {
 
             // new thread
             System.out.println("new thread...");
-            stopWatch.start("new thread");
             Runnable runnable = () -> {
                 for (int i = 0; i < loops; i++) {
                     tasks.task1();
                 }
             };
             Thread thread1 = new Thread(runnable);
+            stopWatch.start("new thread");
             thread1.start();
             thread1.join();
+            stopWatch.stop();
+
+            // completable future simple
+            System.out.println("completable future simple...");
+            stopWatch.start("completable future simple");
+            CompletableFuture.runAsync(() -> {
+                for (int i = 0; i < loops; i++) {
+                    tasks.task1();
+                }
+            }).get();
             stopWatch.stop();
 
             System.out.println(stopWatch.prettyPrint());
